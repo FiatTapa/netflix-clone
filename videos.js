5882f31a338768629b8602a393d3da50ff3d20dc
Yousuf Kalim <yousuf@falconit.com.pk>
2023-07-27 00:49:05 +0500

    Update config and videos path

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
-export const videosPath = './full_video.mp4'
+export const videosPath = '/netflix-clone/full_video.mp4';
 
-export const liveVideoPath ='./live_video.mp4'
+export const liveVideoPath = '/netflix-clone/live_video.mp4';
 
-export const videoRecapPath ='./video_recap.mp4'
+export const videoRecapPath = '/netflix-clone/video_recap.mp4';
